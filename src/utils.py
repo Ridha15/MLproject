581370be1c5ed8f1d4936f79fa73edd6195bf740
Ridha15 <ridha.ds22@duk.ac.in>
2023-10-15 18:10:09 +0530

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+
 
 def save_object(file_path, obj):
     try:
@@ -29,7 +29,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             
             model.fit(X_train,y_train)
 
-     
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -42,6 +42,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
